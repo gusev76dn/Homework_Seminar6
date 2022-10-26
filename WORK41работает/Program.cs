c00f41a3c11eaead5478c040ca12042d8dd3a570
gusev76 <gusev76dn@mail.ru>
2022-10-26 21:56:35 +0300

    добавили папку WORK41работает

---
+++ WORK41работает/Program.cs
@@ -0,0 +1,13 @@
+﻿// задача 41 (работает)
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов больше 0: {count}");
